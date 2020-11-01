ba90cb08dd0b2bd023a20b2d7aa4d7d0cec7cd83
Igor-Polonskiy <53859088+Igor-Polonskiy@users.noreply.github.com>
2020-11-01 23:31:25 +0300

    new boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,20 @@ module.exports = {
    },
    module: {
       rules: [{
-         test:/\.(s*)css$/,
-         use: [
-            miniCss.loader,
-            'css-loader',
-            'sass-loader',
-         ]
-      }]
+            test: /\.(s*)css$/,
+            use: [
+               miniCss.loader,
+               'css-loader',
+               'sass-loader',
+            ]
+         },
+         {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [{
+               loader: 'file-loader',
+            }, ],
+         },
+      ]
    },
    plugins: [
       new miniCss({
